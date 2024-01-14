447148cef60863022bb9125e3ca9f61793a5f78f
Claudio Dornelles <dornelles.crtd@gmail.com>
2024-01-14 10:55:46 -0300

    Initial commit

---
+++ src/main/java/com/example/springbootmeasureexecutiontime/SpringBootMeasureExecutionTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmeasureexecutiontime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMeasureExecutionTimeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootMeasureExecutionTimeApplication.class, args);
+  }
+
+}
